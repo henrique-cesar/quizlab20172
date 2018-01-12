3a3c6f5978e77424d2b457626481b1e7973ea359
Yasmmin Claudino <yayamonteiro97@gmail.com>
2018-01-12 14:25:43 -0300

    primeiros ajustes

---
+++ js/questions.js
@@ -0,0 +1,41 @@
+var questions = [{
+  question: "É característica da indústria 4.0:",
+  choices: ['Centralização de tomada de decisões', 'Sistemas de informações autônomos', 'Utilização dos bancos de dados locais', 'Sistema de produção contínua'],
+  correctAnswer: 1,
+}, {
+  question: "Qual era o nome da aldeia localizada onde Olinda é hoje?",
+  choices: ['Guarani','Marim','Kaiowá','Wajãpi'],
+  correctAnswer: 1,
+}, {
+  question: "Quantas bacias hidrográficas Olinda possui?",
+  choices: [1, 'Nenhuma', 2, 3],
+  correctAnswer: 2,
+}, {
+  question: "O que é esteganografia?",
+  choices: ['Técnica que consiste em esconder um arquivo dentro de outro, de forma criptografada', 'Método que utiliza fontes distintas para organizar códigos de programação.', 'Forma de envio de mensagens criptografadas que são autodestruídas após descobertas.', 'Captura e análise de dados provenientes de mensagens de texto.'],
+  correctAnswer: 0,
+}, {
+  question: "São sistemas computacionais completos e independentes, mais simples que um computador de propósito geral (desktops), encarregados de executar apenas tarefas pré-determinadas:",
+  choices: ['Sistemas binários.', 'Sistemas lineares.', 'Sistemas processuais.', 'Sistemas embarcados.'],
+  correctAnswer: 3,
+}, {
+  question: "São sistemas computacionais completos e independentes, mais simples que um computador de propósito geral (desktops), encarregados de executar apenas tarefas pré-determinadas:",
+  choices: ['Sistemas binários.', 'Sistemas lineares.', 'Sistemas processuais.', 'Sistemas embarcados.'],
+  correctAnswer: 3,
+}, {
+  question: "São sistemas computacionais completos e independentes, mais simples que um computador de propósito geral (desktops), encarregados de executar apenas tarefas pré-determinadas:",
+  choices: ['Sistemas binários.', 'Sistemas lineares.', 'Sistemas processuais.', 'Sistemas embarcados.'],
+  correctAnswer: 3,
+}, {
+  question: "São sistemas computacionais completos e independentes, mais simples que um computador de propósito geral (desktops), encarregados de executar apenas tarefas pré-determinadas:",
+  choices: ['Sistemas binários.', 'Sistemas lineares.', 'Sistemas processuais.', 'Sistemas embarcados.'],
+  correctAnswer: 3,
+}, {
+  question: "São sistemas computacionais completos e independentes, mais simples que um computador de propósito geral (desktops), encarregados de executar apenas tarefas pré-determinadas:",
+  choices: ['Sistemas binários.', 'Sistemas lineares.', 'Sistemas processuais.', 'Sistemas embarcados.'],
+  correctAnswer: 3,
+}, {
+  question: "São sistemas computacionais completos e independentes, mais simples que um computador de propósito geral (desktops), encarregados de executar apenas tarefas pré-determinadas:",
+  choices: ['Sistemas binários.', 'Sistemas lineares.', 'Sistemas processuais.', 'Sistemas embarcados.'],
+  correctAnswer: 3,
+}];
